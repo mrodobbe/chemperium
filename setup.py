2b18ec1e8facdff342f25abcacccc7f83298a149
mrodobbe <mrodobbe.Dobbelaere@UGent.be>
2024-04-21 17:37:15 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='chemperium',
+    version='0.1',
+    packages=['chemperium', 'caesar-data'],
+    url='',
+    license='',
+    author='Maarten Dobbelaere',
+    author_email='mrodobbe.Dobbelaere@UGent.be',
+    description=''
+)
