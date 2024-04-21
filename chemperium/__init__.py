5eb17cd2c32a7b40b1614262ab16bdfa12308776
mrodobbe <mrodobbe.Dobbelaere@UGent.be>
2024-04-21 17:41:23 +0200

    remove init statements

--- chemperium/__init__.py
+++ chemperium/__init__.py
@@ -1,6 +1,1 @@
-import chemperium.data
-import chemperium.features
-import chemperium.molecule
-import chemperium.model
-import chemperium.training
 from chemperium.training.predict import Thermo, Liquid
