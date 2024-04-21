43d19c3ec8a67f1e9dea9691f1a5149e3883aa3b
mrodobbe <mrodobbe.Dobbelaere@UGent.be>
2024-04-21 17:54:51 +0200

    add init to each folder

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(
     name='chemperium',
     version='0.1',
-    packages=['chemperium', 'caesar-data', 'chemperium.training'],
+    packages=['chemperium', 'caesar-data',
+              'chemperium.training', 'chemperium.data', 'chemperium.molecule',
+              'chemperium.model', 'chemperium.postprocessing', 'chemperium.features'],
     url='',
     license='',
     author='Maarten Dobbelaere',
