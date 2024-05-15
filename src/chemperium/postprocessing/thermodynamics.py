4cee2e6be78cafb1c70cb3102f4ecee8cf70c5d6
mrodobbe <mrodobbe.Dobbelaere@UGent.be>
2024-05-15 17:13:05 +0200

    add annotations to thermodynamics.py

--- src/chemperium/postprocessing/thermodynamics.py
+++ src/chemperium/postprocessing/thermodynamics.py
@@ -1,36 +1,60 @@
 import numpy as np
 from scipy.optimize import curve_fit
 from rdkit.Chem.rdchem import Mol
+from typing import Union, Dict
+import numpy.typing as npt
 
 
-def cp_fit(temperatures, a1, a2, a3, a4, a5):
+def cp_fit(temperatures: Union[npt.NDArray[np.float64], float],
+           a1: float,
+           a2: float,
+           a3: float,
+           a4: float,
+           a5: float) -> Union[npt.NDArray[np.float64], float]:
     return a1 + a2 * temperatures + a3 * temperatures ** 2 + a4 * temperatures ** 3 + a5 * temperatures ** 4
 
 
-def enthalpy_fit(temperatures, a1, a2, a3, a4, a5, a6):
+def enthalpy_fit(temperatures: Union[npt.NDArray[np.float64], float],
+                 a1: float,
+                 a2: float,
+                 a3: float,
+                 a4: float,
+                 a5: float,
+                 a6: float) -> Union[npt.NDArray[np.float64], float]:
     return a1 * temperatures + (a2 / 2) * temperatures ** 2 + (a3 / 3) * temperatures ** 3 + \
         (a4 / 4) * temperatures ** 4 + (a5 / 5) * temperatures ** 5 + a6
 
 
-def entropy_fit(temperatures, a1, a2, a3, a4, a5, a7):
+def entropy_fit(temperatures: Union[npt.NDArray[np.float64], float],
+                a1: float,
+                a2: float,
+                a3: float,
+                a4: float,
+                a5: float,
+                a7: float) -> Union[npt.NDArray[np.float64], float]:
     return a1 * np.log(temperatures) + a2 * temperatures + (a3 / 2) * temperatures ** 2 + \
         (a4 / 3) * temperatures ** 4 + (a5 / 5) * temperatures ** 5 + a7
 
 
-def get_cp_coefficients(temperatures, cp_values):
+def get_cp_coefficients(temperatures: Union[float, npt.NDArray[np.float64]],
+                        cp_values: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
     if len(cp_values.shape) > 1:
         coefficients = []
         for i in range(cp_values.shape[0]):
             popt, _ = curve_fit(cp_fit, temperatures, cp_values[i])
             coefficients.append(popt)
-        coefficients = np.array(coefficients).T
+        coefficients_array = np.array(coefficients).T
     else:
         popt, _ = curve_fit(cp_fit, temperatures, cp_values)
-        coefficients = np.array(popt)
-    return coefficients
+        coefficients_array = np.array(popt)
+
+    return coefficients_array
 
 
-def get_nasa_coefficients(temperatures, h298, s298, cp_values):
+def get_nasa_coefficients(temperatures: float,
+                          h298: Union[npt.NDArray[np.float64]],
+                          s298: Union[npt.NDArray[np.float64]],
+                          cp_values: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
     a1, a2, a3, a4, a5 = get_cp_coefficients(temperatures, cp_values * (4.184 / 8.314))
     a6 = h298 * (4.184 / 8.314) - enthalpy_fit(298.15, a1, a2, a3, a4, a5, 0)
     a7 = s298 * (4.184 / 8.314) - entropy_fit(298.15, a1, a2, a3, a4, a5, 0)
@@ -40,8 +64,12 @@ def get_nasa_coefficients(temperatures, h298, s298, cp_values):
         return np.array([a1, a2, a3, a4, a5, a6[0], a7[0]])
 
 
-def get_chemkin_file(name: str, smiles: str, method: str, mol: Mol, nasa_coefficients: np.ndarray):
-    atom_dict = {}
+def get_chemkin_file(name: str,
+                     smiles: str,
+                     method: str,
+                     mol: Mol,
+                     nasa_coefficients: npt.NDArray[np.float64]) -> str:
+    atom_dict = {}  # type: Dict[str, int]
     for atom in mol.GetAtoms():
         sym = atom.GetSymbol()
         if sym in atom_dict:
@@ -80,6 +108,6 @@ def get_chemkin_file(name: str, smiles: str, method: str, mol: Mol, nasa_coeffic
     return chemkin
 
 
-def format_array(arr, decimals=8):
+def format_array(arr: npt.NDArray[np.float64], decimals: int = 8) -> str:
     formatted_arr = np.array2string(arr, formatter={'float_kind': lambda x: ("{: ." + str(decimals) + "e}").format(x)})
     return formatted_arr.replace('\n', ', ')
