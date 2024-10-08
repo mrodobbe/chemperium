cd7bdf06a0b967cff37db5b8ddc207eca45561db
mrodobbe <mrodobbe.Dobbelaere@UGent.be>
2024-10-09 00:26:23 +0200

    add testing for python 3.12

--- tests/test_train.py
+++ tests/test_train.py
@@ -15,3 +15,42 @@ def run_training():
 
     cp.training.train.train(example_file, prop, save_dir, input_arguments)
     print("Training test finished successfully!")
+
+
+def run_training_fp():
+    example_file = "../test_data/test_example.csv"
+    prop = "magic_property"
+    save_dir = "../test_data/test_output"
+    input_arguments = {"batch": 16,
+                       "seed": 123456789,
+                       "epochs": 2,
+                       "patience": 1,
+                       "depth": 4,
+                       "folds": 1,
+                       "num_layers": 2,
+                       "fingerprint": "morgan"}
+
+    cp.training.train.train(example_file, prop, save_dir, input_arguments)
+    print("Training test finished successfully!")
+
+
+def run_training_gaul():
+    example_file = "../test_data/test_example.csv"
+    prop = "magic_property"
+    save_dir = "../test_data/test_output"
+    input_arguments = {"batch": 16,
+                       "seed": 123456789,
+                       "epochs": 2,
+                       "patience": 1,
+                       "depth": 4,
+                       "folds": 1,
+                       "num_layers": 2,
+                       "distances": True,
+                       "angles": True,
+                       "dihedrals": True,
+                       "include_3d": True,
+                       "ff_3d": True,
+                       "fingerprint": "hdad"}
+
+    cp.training.train.train(example_file, prop, save_dir, input_arguments)
+    print("Training test finished successfully!")
